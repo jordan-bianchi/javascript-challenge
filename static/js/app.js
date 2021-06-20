dce4c95bfb343cc2db0b9859f1d9eb920ce5cbf7
jordan_bianchi <jordan_bianchi@yahoo.com>
2021-06-19 21:39:01 -0400

    created table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// from data.js
+var tableData = data;
+
+// Get reference to table body
+var tbody = d3.select("tbody");
+
+// Append table to web page and add rows of data for each UFO sighting
+tableData.forEach(ufoSightings => {
+
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSightings).forEach(([key,value]) => {
+
+        console.log(key,value);
+        var cell = row.append("td").text(value)
+        
+    });
+});
